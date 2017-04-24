8960278865ba43697dd582ff27e2ea7c43e9bb4b
Marina Hoashi <marinahoashi@gmail.com>
2017-04-24 12:58:29 -0400

    Finished up to bonus section

--- bash.js
+++ bash.js
@@ -1,20 +1,39 @@
 var commands = require('./commands');
-var fs = require('fs');
+var chalk = require('chalk');
 
 // Output a prompt
-process.stdout.write('prompt > ');
+process.stdout.write(chalk.yellow(('prompt > ')));
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-
-  commands.pwd(cmd);
-  commands.ls(cmd);
-
-  if (cmd === 'date') {
-    process.stdout.write(Date());
+  var command = cmd.split(' ')[0];
+  var args = cmd.split(' ').slice(1).join(' ');
+  // var output;
+  if (command === 'pwd') {
+    commands.pwd('.', done);
   }
-
-  process.stdout.write('\nprompt > ');
-
+  if (command === 'ls') {
+    commands.ls(args, done);
+  }
+  if (command === 'date') {
+    commands.date('.', done);
+  }
+  if (command === 'echo') {
+    commands.echo(args, done);
+  }
+  if (command === 'cat') {
+    commands.cat(args, done);
+  }
+  if (command === 'head') {
+    commands.head(args, done);
+  }
+  if (command === 'tail') {
+    commands.tail(args, done);
+  }
 });
+
+var done = function(output) {
+  process.stdout.write(chalk.green(output));
+  process.stdout.write(chalk.yellow(('\nprompt > ')));
+}
--- commands.js
+++ commands.js
@@ -1,17 +1,80 @@
-var pwd = function(cmd) {
-  if (cmd === 'pwd') {
-    process.stdout.write(__dirname);
-  }
+var fs = require('fs');
+var chalk = require('chalk');
 
+var pwd = function(file, done) {
+  var output = __dirname;
+  done(output);
 }
 
-var ls = function(cmd) {
-  if(cmd === 'ls') {
-    process.stdout.write(fs.readdirSync(__dirname));
-  }
+var ls = function(file, done) {
+  var dir = file || '.';
+  var output;
+  return fs.readdir(dir, function(err, data){
+    if (err) throw err;
+    output = data.toString().split(',').join(' ');
+    done(output);
+  });
 }
+
+var date = function(file, done) {
+  var output =  Date();
+  done(output);
+}
+
+var echo = function(file, done) {
+  var output;
+  if (file[0] === '$') {
+    var envVariable = file.slice(1);
+    output = process.env[envVariable];
+  } else {
+    output = file;
+  }
+  done(output);
+}
+
+var cat = function(file, done) {
+  var output;
+  var fileNames = file.split(' ');
+  var numReadFiles = 0;
+  fileNames.forEach(function(name) {
+    fs.readFile(name, function(err, data){
+      if (err) throw err;
+      output += data;
+      numReadFiles++;
+      if (numReadFiles === fileNames.length) done(output);
+    });
+  });
+}
+
+var head = function(file, done) {
+  var output;
+  return fs.readFile(file, function(err, data){
+    if(err) throw err;
+
+    output = data.toString().split("\n").slice(0,5).join("\n");
+    done(output);
+  });
+}
+
+var tail = function(file, done) {
+  var output;
+  return fs.readFile(file, function(err, data){
+    if(err) throw err;
+    var lines = data.toString().split("\n")
+    var startLine = lines.length < 5 ? 0 : lines.length - 5;
+    output = lines.slice(startLine).join("\n");
+    done(output);
+  });
+}
+
+
 
 module.exports = {
   pwd : pwd,
   ls : ls,
+  date: date,
+  echo: echo,
+  cat: cat,
+  head: head,
+  tail: tail,
 }
